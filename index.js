d83d5949f9f2f23c3919c000137a4f61331369cf
aaliymn <48835782+aaliymn@users.noreply.github.com>
2023-10-18 16:47:12 +0300

    main

---
+++ index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+let allNotes = [];
+let workNotes = [];
+
+function doneTask() {}
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { today, allNotes });
+});
+app.get("/work", (req, res) => {
+  res.render("work.ejs", { today, workNotes });
+});
+
+app.post("/submit", (req, res) => {
+  allNotes.push(req.body["usernote"]);
+  res.render("index.ejs", { allNotes, today });
+  console.log(allNotes.length);
+});
+app.post("/submitw", (req, res) => {
+  workNotes.push(req.body["usernote"]);
+  res.render("work.ejs", { workNotes, today });
+  console.log(workNotes.length);
+});
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let today =
+  days[new Date().getDay()] +
+  ", " +
+  month[new Date().getMonth()] +
+  " " +
+  new Date().getDate();
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
